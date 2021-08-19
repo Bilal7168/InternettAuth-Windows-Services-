9f0a9b6d5172be651f706a6d42cd2056bb97a5a1
Bilal Shahid <Bilal Shahid@DESKTOP-K9AH6NS>
2021-08-19 19:45:43 +0500

    Add project files.

---
+++ HttpKeyGenerator (Windows Services)/GenerateKey.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.IO;
+
+namespace InternetAuth__Windows_Services_
+{
+    public class GenerateKey
+    {
+        private readonly Timer _checker;
+        private int executeCode = 2;
+        private bool isBlocked = true;
+        private bool netCutOff = false;
+        
+        public GenerateKey()
+        {
+            _checker = new Timer(1000); //creating the timer for every second
+            _checker.AutoReset = true; //make sure it resets after every elapse
+            _checker.Elapsed += _checker_Elapsed; //the function that happens whenever the time reaches 1 second
+        }
+
+        private void _checker_Elapsed(object sender, ElapsedEventArgs e) //the part where we check from the text file
+        {
+
+            switch (executeCode)
+            {
+                case 0:
+                    //execute the first check
+                    //cut off the internet connection
+                    Process.Start("ipconfig", "/release");
+
+                    //log error
+                    using (StreamWriter writer = File.AppendText(".\\ServiceLog.txt"))
+                    {
+                        writer.WriteLine("Internet has been blocked at time: '" + DateTime.Now.ToString() + "'");
+                        writer.Close();
+                    }
+                    netCutOff = true;
+                    isBlocked = false;
+                    executeCode = 2; //make it go towards case 2
+                    break;
+
+                case 1:
+                    Process.Start("ipconfig", "/renew");
+                    using (StreamWriter writer = File.AppendText(".\\ServiceLog.txt"))
+                    {
+                        writer.WriteLine("Internet Conn. Service Started at time: '" + DateTime.Now.ToString() + "'");
+                        writer.Close();
+                    }
+                    executeCode = 2;
+                    netCutOff = false;
+                    break;
+
+                case 2:
+                    using (StreamReader sr = new StreamReader(".\\BlockedUser.txt"))
+                    {
+                        string readLine;
+                        while ((readLine = sr.ReadLine()) != null)
+                        {
+                            if (Environment.MachineName.ToString() == readLine && !netCutOff) //the desktop name exists in the list of 'Blocked Users'
+                            {
+                                executeCode = 0; //make it go towards disabling internet
+                            }
+                        }
+                    }
+                    using (StreamReader sr = new StreamReader(".\\BlockedUser.txt"))
+                    {
+                        string readLine;
+                        while ((readLine = sr.ReadLine()) != null)
+                        {
+                            if (Environment.MachineName.ToString() != readLine && netCutOff && !isBlocked)
+                            {
+                                executeCode = 1; //towards enabling internet
+                            }
+                        }
+                        break;
+                    }
+
+            }
+        }
+
+        //....(1)
+
+        public void Stop()
+        {
+            _checker.Stop();
+        }
+        public void Start()
+        {
+            _checker.Start();
+        }
+    }
+}
---
+++ HttpKeyGenerator (Windows Services)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace InternetAuth__Windows_Services_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<GenerateKey>(s =>
+               {
+                   s.ConstructUsing(GenerateKey => new GenerateKey());
+                   s.WhenStarted(GenerateKey => GenerateKey.Start());
+                   s.WhenStopped(GenerateKey => GenerateKey.Stop());
+               });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("UserInternetControl");
+                x.SetDisplayName("Internet Regulator"); //change name to display service as a hidden -ware
+                x.SetDescription("Used to authenticate Internet access to User");
+            });
+
+        }
+    }
+}
